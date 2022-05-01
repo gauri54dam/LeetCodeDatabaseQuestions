2ae61f995dc30a207353f3f1c8291946fb755c1e
Gauri Kishor Damle <gauri54dam@gmail.com>
2022-04-30 17:37:31 -0700

    Add files via upload

---
+++ Day1_Select/1757_RecyclableAndLowFatProducts.sql
@@ -0,0 +1,15 @@
+/* 
+
+product_id is the primary key for Products table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+*/
+
+-- Write an SQL query to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+
+
+select
+    product_id
+from Products p
+where low_fats = 'Y' and recyclable = 'Y'
