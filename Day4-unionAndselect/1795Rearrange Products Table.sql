39ce24187d6034545e4ac96f15fc5724b88e404a
Gauri Kishor Damle <gauri54dam@gmail.com>
2022-05-04 16:46:40 -0700

    Add files via upload

---
+++ Day4-unionAndselect/1795Rearrange Products Table.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+
+select 
+product_id, store, price
+from
+    (
+        select 
+            product_id, 
+            store1, 
+            store2, 
+            store3 
+        from Products 
+    ) sourcetb
+unpivot
+(price for store in (store1, store2, store3)) as unpivottb;
