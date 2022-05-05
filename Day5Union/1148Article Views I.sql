9ac3cf41d00e05561ddfe6f99b9712e107874eeb
Gauri Kishor Damle <gauri54dam@gmail.com>
2022-05-04 19:07:12 -0700

    Add files via upload

---
+++ Day5Union/1148Article Views I.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+
+select distinct
+    author_id as id
+from Views
+where author_id = viewer_id
+order by 1
+
+
---
+++ Day5Union/175Combine Two Tables.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+
+select 
+    firstName, 
+    lastName, 
+    city, 
+    state
+from Person p
+left join Address a
+on p.personId = a.personId
