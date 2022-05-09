6f31cae4a3d9365aea4aa89e59eebe90534133f3
Gauri Kishor Damle <gauri54dam@gmail.com>
2022-05-08 23:37:18 -0700

    Add files via upload

---
+++ Day 9ControlFlow/1393.Capital GainLoss.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+
+select 
+    stock_name,
+    sum(case 
+        when operation = 'Sell' then (price)
+       else (price*-1) end) as capital_gain_loss
+from Stocks 
+group by stock_name
