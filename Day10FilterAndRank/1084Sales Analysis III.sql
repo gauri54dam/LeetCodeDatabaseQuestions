8c38c8059527f9e49a0f057b855d5e20020c3296
Gauri Kishor Damle <gauri54dam@gmail.com>
2022-05-08 22:26:37 -0700

    Add files via upload

---
+++ Day10FilterAndRank/1084Sales Analysis III.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+select 
+    product_id,
+    product_name
+from Product 
+where product_id not in 
+(
+    select product_id 
+    from Sales
+    where sale_date not between '2019-01-01' and '2019-03-31'
+)
